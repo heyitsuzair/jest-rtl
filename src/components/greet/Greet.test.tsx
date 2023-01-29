a5671b7a7bcac3204ccbc63ae7efe37e1a3a8368
Muhammad Uzair <uzairdevil354123@gmail.com>
2023-01-30 03:46:28 +0500

    Grouping Tests

--- src/components/greet/Greet.test.tsx
+++ src/components/greet/Greet.test.tsx
@@ -1,14 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import Greet from "./Greet";
 
-test("Greet Renders Correctly", () => {
-  render(<Greet />);
-  const textElement = screen.getByText("Hello");
-  expect(textElement).toBeInTheDocument();
+describe("Greet", () => {
+  test("Renders Correctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText("Hello");
+    expect(textElement).toBeInTheDocument();
+  });
 });
-
-test("Greet Renders Correctly With Name", () => {
-  render(<Greet name="Uzair" />);
-  const textElement = screen.getByText("Hello Uzair");
-  expect(textElement).toBeInTheDocument();
+describe("nested", () => {
+  test("Renders Correctly With Name", () => {
+    render(<Greet name="Uzair" />);
+    const textElement = screen.getByText("Hello Uzair");
+    expect(textElement).toBeInTheDocument();
+  });
 });
