ce8a2f493f9f614b586ebf58c8ed5b2513a4a5ea
Muhammad Uzair <uzairdevil354123@gmail.com>
2023-01-31 06:50:01 +0500

    getByRole

--- src/components/greet/Greet.test.tsx
+++ src/components/greet/Greet.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import { text } from "node:stream/consumers";
 import Greet from "./Greet";
 
 describe("Greet", () => {
@@ -6,5 +7,26 @@ describe("Greet", () => {
     render(<Greet />);
     const textElement = screen.getByText(/Hello/);
     expect(textElement).toBeInTheDocument();
+  });
+  test("Renders Input Correctly", () => {
+    render(<Greet />);
+    const inputElement = screen.getByRole("textbox", {
+      name: "Email",
+    });
+    expect(inputElement).toBeInTheDocument();
+
+    const textArea = screen.getByRole("textbox", {
+      name: "Description",
+    });
+    expect(textArea).toBeInTheDocument();
+
+    const pageHeading = screen.getByRole("heading", {
+      level: 1,
+    });
+    const pageSubHeading = screen.getByRole("heading", {
+      level: 2,
+    });
+    expect(pageHeading).toBeInTheDocument();
+    expect(pageSubHeading).toBeInTheDocument();
   });
 });
--- src/components/greet/Greet.tsx
+++ src/components/greet/Greet.tsx
@@ -1,7 +1,17 @@
 import { GreetProps } from "./Greet.types";
 
 const Greet = ({ name }: GreetProps) => {
-  return <div>Hello {name ? name : "Guest"}</div>;
+  return (
+    <div>
+      <h1>Page Heading</h1>
+      <h2>Page Sub Heading</h2>
+      Hello {name ? name : "Guest"}
+      <label htmlFor="email">Email</label>
+      <input type="text" placeholder="Enter Email" id="email" />
+      <label htmlFor="description">Description</label>
+      <textarea id="description"></textarea>
+    </div>
+  );
 };
 
 export default Greet;
